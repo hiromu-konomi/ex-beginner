90c6254c2b1d72c2389fdfb9a2829700eec48a46
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-06-16 16:32:52 +0900

    exam05-result.htmlの作成とコントローラの修正

--- src/main/java/com/example/controller/Exam05Controller.java
+++ src/main/java/com/example/controller/Exam05Controller.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.service.MemberService;
@@ -13,9 +14,14 @@ public class Exam05Controller {
 	@Autowired
 	private MemberService service;
 	
+	@RequestMapping("")
+	public String index() {
+		return "exam05";
+	}
+	
 	@RequestMapping("/input-form")
-	public String input(String name) {
-		service.findBy(name).forEach(System.out::println);
+	public String input(String name, Model model) {
+		model.addAttribute("nameList", service.findBy(name));
 		return "exam05-result";
 	}
 }
